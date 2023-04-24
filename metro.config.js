438f695ffdca02f816e76b507ed95e6f0357977f
FacundoSantino <sanchezfacundosantino@gmail.com>
2023-04-24 12:22:03 -0300

    Primer commit

---
+++ metro.config.js
@@ -0,0 +1,20 @@
+/**
+ * Metro configuration for React Native
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+module.exports = {
+  resolver: {
+    assetExts: ['png', 'jpg', 'jpeg', 'gif', 'svg', 'mp4', 'aac', 'wav', 'ttf', 'otf', 'woff'],
+  },
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: true,
+      },
+    }),
+  },
+};
