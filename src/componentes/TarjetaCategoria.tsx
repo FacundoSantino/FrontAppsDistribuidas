a90bc134122cbe9e7b1c01e7e0a2b6fdc163f48a
FacundoSantino <sanchezfacundosantino@gmail.com>
2023-05-09 14:36:51 -0300

    ADD componente cards categorías

---
+++ src/componentes/TarjetaCategoria.tsx
@@ -0,0 +1,50 @@
+import {Image, ImageSourcePropType, View, Text} from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+
+type TarjetaCategoriaProps ={
+    nombre: String;
+    onPress(): void;
+    sourceFoto: ImageSourcePropType;
+    colorInterno: Color;
+    colorExterno: Color;
+    paddingTop: number;
+    paddingBottom:number;
+    paddingHorizontal: number;
+    ancho:number;
+}
+
+type RGB = `rgb(${number}, ${number}, ${number})`;
+type RGBA = `rgba(${number}, ${number}, ${number}, ${number})`;
+type HEX = `#${string}`;
+
+type Color = RGB | RGBA | HEX;
+
+const TarjetaCategoria= (props:TarjetaCategoriaProps) =>{
+    return(
+        <TouchableOpacity style={{backgroundColor:props.colorExterno,paddingTop:props.paddingTop,
+            paddingBottom:props.paddingBottom,borderColor:"#000000", borderWidth:1,
+            paddingHorizontal:props.paddingHorizontal, borderRadius:20, width:props.ancho,
+            marginTop:20,
+            }} onPress={(props.onPress)}>
+
+            <View style={{backgroundColor:props.colorInterno,        
+                display:"flex",
+                flexDirection: "row",
+                alignItems:"center",
+                borderRadius:15,
+                padding:10,
+                
+                }}>
+                
+                <Image source={props.sourceFoto} style={{justifyContent:"flex-start"}} />
+
+                <Text style={{fontSize:24}}> {props.nombre} </Text>
+
+            </View>
+            
+        </TouchableOpacity>
+    )
+
+}
+
+export default TarjetaCategoria;
--- src/pantallas/Home.tsx
+++ src/pantallas/Home.tsx
@@ -7,6 +7,11 @@ import MorfAr from "../assets/MorfAR.png";
 import lupa from "../assets/lupa.png";
 import LogoSol from "../assets/Logo_Sol_Chico.png";
 import estiloApp from "../estilos/estiloApp";
+import TarjetaCategoria from "../componentes/TarjetaCategoria";
+import fotoMisRecetas from "../assets/mis_recetas.png";
+import fotoMiLista from "../assets/mi_lista.png";
+import fotoCategorias from "../assets/categorias.png";
+
 
 function Home(): JSX.Element{
 
@@ -35,6 +40,49 @@ function Home(): JSX.Element{
                         <Image source={lupa} style={style.elemento} />
 
                         <TextInput style={style.elemento} placeholder="Ingresá tu busqueda..." />
+
+                    </View>
+
+                    <View style={style.flexColumn}>
+
+                        <TarjetaCategoria 
+                        nombre={"MIS RECETAS"} 
+                        onPress={function (): void {
+                            console.log("Mis recetas");
+                        } } 
+                        sourceFoto={fotoMisRecetas} 
+                        colorInterno={"#FCB826"} 
+                        colorExterno={"#FFFDFD"} 
+                        paddingTop={10}
+                        paddingBottom={24} 
+                        ancho={334}
+                        paddingHorizontal={13}/>
+
+                        <TarjetaCategoria 
+                        nombre={"MI LISTA"} 
+                        onPress={function (): void {
+                            console.log("Mi lista");
+                        } } 
+                        sourceFoto={fotoMiLista} 
+                        colorInterno={"#FCB826"} 
+                        colorExterno={"#FFFDFD"} 
+                        paddingTop={10}
+                        paddingBottom={24}  
+                        ancho={334}
+                        paddingHorizontal={13}/>
+
+                        <TarjetaCategoria 
+                        nombre={"CATEGORIAS"} 
+                        onPress={function (): void {
+                            console.log("Categorías");
+                        } } 
+                        sourceFoto={fotoCategorias} 
+                        colorInterno={"#FCB826"} 
+                        colorExterno={"#FFFDFD"} 
+                        paddingTop={10}
+                        paddingBottom={24}  
+                        ancho={334}
+                        paddingHorizontal={13}/>
 
                     </View>
                     
