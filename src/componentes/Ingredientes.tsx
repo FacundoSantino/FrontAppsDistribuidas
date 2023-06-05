3fbf4e4d283869d13b8d1ed9395d464509affff5
FacundoSantino <sanchezfacundosantino@gmail.com>
2023-06-05 11:35:59 -0300

    ingredientes

--- src/componentes/Ingredientes.tsx
+++ src/componentes/Ingredientes.tsx
@@ -1,6 +1,8 @@
 import { RouteProp, useRoute } from "@react-navigation/native";
 import { TipoParametros } from "../App";
-import { View } from "react-native";
+import { ScrollView, Text, View } from "react-native";
+import PantallaTipoHome from "./PantallaTipoHome";
+import { useState } from "react";
 
 
 type IngredientesProps= RouteProp<TipoParametros, "Receta">;
@@ -8,13 +10,29 @@ type IngredientesProps= RouteProp<TipoParametros, "Receta">;
 
 export default function Ingredientes() :JSX.Element {
 
+    const[ingredientesProcesados,setIngredientesProcesados]=useState(<View></View>);
+    const[comensales,setComensales]=useState(
+        <View></View>
+    )
+
+    
 
     const route=useRoute<IngredientesProps>();
 
-    return(
-        <View>
 
-        </View>
+
+    return(
+        <PantallaTipoHome contenido={
+            <View>
+                <Text style={{textAlign:'center',fontWeight:'bold',fontSize:30}}> Titulo receta </Text>
+                <Text style={{textAlign:'center',color:'black',fontSize:15}}> Ingredientes </Text>
+                <ScrollView>
+                    {comensales}
+                    {ingredientesProcesados}
+                </ScrollView>
+
+            </View>
+        }/>   
     )
 
 }
